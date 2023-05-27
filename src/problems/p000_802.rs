9b23e99fe89f781860a18b655ed4d37f94883001
zhuyk6 <zhuyk6@foxmail.com>
2023-05-28 02:16:43 +0800

    Finish p000_802 and p001_439.

---
+++ src/problems/p000_802.rs
@@ -0,0 +1,45 @@
+
+pub fn eventual_safe_nodes<Out, In>(graph: Out) -> Vec<i32> 
+    where
+        In: AsRef<[i32]>,
+        Out: AsRef<[In]>,
+{
+    let n = graph.as_ref().len();
+    let mut back_edge = vec![vec![]; n];
+    let mut degree = vec![0; n];
+
+    for (i, to) in graph.as_ref().into_iter().enumerate() {
+        for j in to.as_ref() {
+            back_edge[*j as usize].push(i);
+            degree[i] += 1;
+        }
+    }
+
+    use std::collections::VecDeque;
+
+    let mut que = VecDeque::new();
+
+    for (i, d) in degree.iter().enumerate() {
+        if *d == 0 {
+            que.push_back(i);
+        }
+    }
+    let mut safe = vec![];
+    while let Some(x) = que.pop_front() {
+        safe.push(x as i32);
+        for y in back_edge[x].iter() {
+            degree[*y] -= 1;
+            if degree[*y] == 0 {
+                que.push_back(*y);
+            }
+        }
+    }
+    safe.sort();
+    safe
+}
+
+#[test]
+fn example() {
+    let edge = vec![vec![1,2],vec![2,3],vec![5],vec![0],vec![5],vec![7], vec![], vec![]];
+    assert_eq!(eventual_safe_nodes(edge), vec![2,4,5,6,7]);
+}
---
+++ src/problems/p001_439.rs
@@ -0,0 +1,40 @@
+
+pub fn kth_smallest(mat: Vec<Vec<i32>>, k: i32) -> i32 {
+    use std::collections::BinaryHeap;
+    use std::cmp::Reverse;
+    
+
+    fn kth_small_between2(a: Vec<i32>, b: Vec<i32>, mut k: usize) -> Vec<i32> {
+        let mut heap: BinaryHeap<(Reverse<i32>, usize, usize)> = BinaryHeap::new();
+        let mut c = Vec::new();
+
+        let la = a.len();
+        let lb = b.len();
+        for j in 0..lb {
+            heap.push((Reverse(a[0] + b[j]), 0, j));
+        }
+        while let Some((Reverse(sum), i, j)) = heap.pop() {
+            c.push(sum);
+            k -= 1;
+            if k == 0 { break; }
+            
+            if i + 1 < la {
+                heap.push((Reverse(a[i+1] + b[j]), i+1, j));
+            }
+        }
+        c
+    }
+
+    let k = k as usize;
+    *mat.into_iter()
+        .reduce(|a, b| kth_small_between2(a, b, k))
+        .unwrap()
+        .get(k-1).unwrap()
+}
+
+#[test]
+fn example() {
+    let mat = vec![vec![1,3,11],vec![2,4,6]]; 
+    let k = 5;
+    assert_eq!(kth_smallest(mat, k), 7);
+}
