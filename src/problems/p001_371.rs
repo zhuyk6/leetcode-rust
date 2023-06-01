ab05a40310cfbbed1670d3ef64e9a251310f5614
zhuyk6 <zhuyk6@foxmail.com>
2023-06-02 02:22:18 +0800

    Finish p001_371 and p002_559.

---
+++ src/problems/p001_371.rs
@@ -0,0 +1,36 @@
+
+pub fn find_the_longest_substring(s: String) -> i32 {
+    use std::collections::HashMap;
+
+    let mut map: HashMap<[u8; 5], i32> = HashMap::new();
+    let mut acc = [0u8; 5];
+
+    map.insert(acc.clone(), -1);
+
+    let mut ans = 0;
+
+    for (i, c) in s.bytes().enumerate() {
+        match c {
+            b'a' => acc[0] ^= 1,
+            b'e' => acc[1] ^= 1,
+            b'i' => acc[2] ^= 1,
+            b'o' => acc[3] ^= 1,
+            b'u' => acc[4] ^= 1,
+            _ => (),
+        }
+        map.entry(acc.clone()).and_modify(|v| {
+            ans = ans.max(i as i32 - *v);
+        }).or_insert(i as i32);
+    }
+
+    ans
+}
+
+#[test]
+fn example() {
+    let s = "eleetminicoworoep".to_string();
+    assert_eq!(find_the_longest_substring(s), 13);
+
+    let s = "leetcodeisgreat".to_string();
+    assert_eq!(find_the_longest_substring(s), 5);
+}
