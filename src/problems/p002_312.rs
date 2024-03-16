956a29068c689e098f309accbddb2c3312894fc7
zhuyk6 <zhuyk6@foxmail.com>
2024-03-16 20:34:37 +0800

    Finish 2312, 2684.

---
+++ src/problems/p002_312.rs
@@ -0,0 +1,55 @@
+struct Solution;
+
+#[allow(unused)]
+impl Solution {
+    pub fn selling_wood(m: i32, n: i32, prices: Vec<Vec<i32>>) -> i64 {
+        let m = m as usize;
+        let n = n as usize;
+        let prices: Vec<(usize, usize, i64)> = prices
+            .into_iter()
+            .map(|v| (v[0] as usize, v[1] as usize, v[2] as i64))
+            .filter(|(x, y, w)| *x <= m && *y <= n)
+            .collect();
+
+        let mut f = vec![vec![0i64; n + 1]; m + 1];
+        for p in &prices {
+            f[p.0][p.1] = f[p.0][p.1].max(p.2);
+        }
+
+        for i in 1..=m {
+            for j in 1..=n {
+                // println!("f[{}][{}] = {} ", i, j, f[i][j]);
+
+                // using f[i][j] to update f[i+d][j] and f[i][j+d]
+                for d in 1..=(m - i) {
+                    f[i + d][j] = f[i + d][j].max(f[i][j] + f[d][j]);
+                }
+                for d in 1..=(n - j) {
+                    f[i][j + d] = f[i][j + d].max(f[i][j] + f[i][d]);
+                }
+            }
+        }
+
+        f[m][n]
+    }
+}
+
+#[test]
+fn test1() {
+    let m = 3;
+    let n = 5;
+    let prices = [[1, 4, 2], [2, 2, 7], [2, 1, 3]];
+    let prices: Vec<Vec<i32>> = prices.into_iter().map(Vec::from).collect();
+
+    assert_eq!(Solution::selling_wood(m, n, prices), 19);
+}
+
+#[test]
+fn test2() {
+    let m = 4;
+    let n = 6;
+    let prices = [[3, 2, 10], [1, 4, 2], [4, 1, 3]];
+    let prices: Vec<Vec<i32>> = prices.into_iter().map(Vec::from).collect();
+
+    assert_eq!(Solution::selling_wood(m, n, prices), 32);
+}
---
+++ src/problems/p002_684.rs
@@ -0,0 +1,40 @@
+struct Solution;
+
+#[allow(unused, clippy::needless_range_loop)]
+impl Solution {
+    pub fn max_moves(grid: Vec<Vec<i32>>) -> i32 {
+        let m = grid.len();
+        let n = grid[0].len();
+
+        let mut f = vec![vec![0; n]; m];
+
+        for j in (0..(n - 1)).rev() {
+            for i in 0..m {
+                for k in i.saturating_sub(1)..=(i + 1).min(m - 1) {
+                    if grid[i][j] < grid[k][j + 1] {
+                        f[i][j] = f[i][j].max(1 + f[k][j + 1]);
+                    }
+                }
+            }
+        }
+        println!("{:?}", f);
+
+        (0..m).map(|i| f[i][0]).max().unwrap()
+    }
+}
+
+#[test]
+fn test1() {
+    let grid = [[2, 4, 3, 5], [5, 4, 9, 3], [3, 4, 2, 11], [10, 9, 13, 15]];
+    let grid: Vec<Vec<i32>> = grid.into_iter().map(Vec::from).collect();
+
+    assert_eq!(Solution::max_moves(grid), 3);
+}
+
+#[test]
+fn test2() {
+    let grid = [[3, 2, 4], [2, 1, 9], [1, 1, 7]];
+    let grid: Vec<Vec<i32>> = grid.into_iter().map(Vec::from).collect();
+
+    assert_eq!(Solution::max_moves(grid), 0);
+}
