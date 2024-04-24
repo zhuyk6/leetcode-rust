5e3a208b5a8114847cccf937a3705558630d5d6b
zhuyk6 <zhuyk6@foxmail.com>
2024-04-24 14:44:09 +0800

    Finish 2385, 3108.

---
+++ src/problems/p002_385.rs
@@ -0,0 +1,54 @@
+use crate::rctree::TreeNode;
+struct Solution;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+struct Dfs {
+    start: i32,
+    ans: u32,
+}
+
+#[allow(dead_code)]
+impl Dfs {
+    fn dfs(&mut self, x: Option<Rc<RefCell<TreeNode>>>) -> (u32, Option<u32>) {
+        if let Some(node) = x {
+            if node.as_ref().borrow().val == self.start {
+                let (d1, _) = self.dfs(node.as_ref().borrow().left.clone());
+                let (d2, _) = self.dfs(node.as_ref().borrow().right.clone());
+                self.ans = self.ans.max(d1.max(d2));
+
+                (d1.max(d2) + 1, Some(1))
+            } else {
+                let (d1, option1) = self.dfs(node.as_ref().borrow().left.clone());
+                let (d2, option2) = self.dfs(node.as_ref().borrow().right.clone());
+
+                println!("{d1}, {option1:?}");
+                println!("{d2}, {option2:?}");
+
+                if let Some(d0) = option1 {
+                    self.ans = self.ans.max(d0 + 1 + d2);
+
+                    (d1.max(d2) + 1, Some(d0 + 1))
+                } else if let Some(d0) = option2 {
+                    self.ans = self.ans.max(d0 + 1 + d1);
+
+                    (d1.max(d2) + 1, Some(d0 + 1))
+                } else {
+                    (d1.max(d2) + 1, None)
+                }
+            }
+        } else {
+            (0, None)
+        }
+    }
+}
+
+#[allow(dead_code)]
+impl Solution {
+    pub fn amount_of_time(root: Option<Rc<RefCell<TreeNode>>>, start: i32) -> i32 {
+        let mut dfs = Dfs { start, ans: 0 };
+        dfs.dfs(root);
+        dfs.ans as i32
+    }
+}
---
+++ src/problems/p003_108.rs
@@ -0,0 +1,81 @@
+struct Solution;
+
+#[allow(dead_code)]
+impl Solution {
+    pub fn minimum_cost(n: i32, edges: Vec<Vec<i32>>, query: Vec<Vec<i32>>) -> Vec<i32> {
+        let n = n as usize;
+
+        use std::collections::HashMap;
+        let mut graph = vec![HashMap::new(); n];
+        for e in edges {
+            let x = e[0] as usize;
+            let y = e[1] as usize;
+            let c = e[2];
+            graph[x].entry(y).and_modify(|c0| *c0 &= c).or_insert(c);
+            graph[y].entry(x).and_modify(|c0| *c0 &= c).or_insert(c);
+        }
+
+        let mut group_num = 0;
+        let mut group_cost = vec![];
+        let mut belong_group = vec![usize::MAX; n];
+
+        let mut vis = vec![false; n];
+        for i in 0..n {
+            if !vis[i] {
+                use std::collections::VecDeque;
+                let mut que = VecDeque::new();
+                vis[i] = true;
+                que.push_back(i);
+
+                let mut cost = i32::MAX;
+                while let Some(x) = que.pop_front() {
+                    belong_group[x] = group_num;
+                    for (&y, &c) in &graph[x] {
+                        cost &= c;
+                        if !vis[y] {
+                            vis[y] = true;
+                            que.push_back(y);
+                        }
+                    }
+                }
+                group_cost.push(cost);
+                group_num += 1;
+            }
+        }
+
+        query
+            .into_iter()
+            .map(|q| {
+                let x = q[0] as usize;
+                let y = q[1] as usize;
+                if belong_group[x] == belong_group[y] {
+                    group_cost[belong_group[x]]
+                } else {
+                    -1
+                }
+            })
+            .collect()
+    }
+}
+
+#[test]
+fn test1() {
+    let n = 5;
+    let edges = [[0, 1, 7], [1, 3, 7], [1, 2, 1]];
+    let query = [[0, 3], [3, 4]];
+    let edges: Vec<Vec<i32>> = edges.into_iter().map(Vec::from).collect();
+    let query: Vec<Vec<i32>> = query.into_iter().map(Vec::from).collect();
+
+    assert_eq!(Solution::minimum_cost(n, edges, query), vec![1, -1]);
+}
+
+#[test]
+fn test2() {
+    let n = 3;
+    let edges = [[0, 2, 7], [0, 1, 15], [1, 2, 6], [1, 2, 1]];
+    let query = [[1, 2]];
+    let edges: Vec<Vec<i32>> = edges.into_iter().map(Vec::from).collect();
+    let query: Vec<Vec<i32>> = query.into_iter().map(Vec::from).collect();
+
+    assert_eq!(Solution::minimum_cost(n, edges, query), vec![0]);
+}
